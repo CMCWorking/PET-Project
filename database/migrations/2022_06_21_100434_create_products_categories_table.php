01c70788020c4cb1b3a2b0d40f4d1cec58717118
Cam <github@lekienhoanh.dev>
2022-06-21 17:05:35 +0700

    create Products Categories model and Migration

---
+++ database/migrations/2022_06_21_100434_create_products_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('product_id')->nullable();
+            $table->unsignedInteger('category_id')->nullable();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_categories');
+    }
+}
