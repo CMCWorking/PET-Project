bbf55cbdc70c0d786cb31dc291dd902919147ec4
Cam <github@lekienhoanh.dev>
2022-06-21 17:28:58 +0700

    Customer Information require unique() phone

--- database/migrations/2022_06_21_095641_create_customer_informations_table.php
+++ database/migrations/2022_06_21_095641_create_customer_informations_table.php
@@ -17,7 +17,7 @@ class CreateCustomerInformationsTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone');
+            $table->string('phone')->unique();
             $table->string('password');
             $table->integer('receive_promotion');
             $table->string('login_engine');
