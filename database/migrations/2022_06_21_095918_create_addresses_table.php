dbbdc24af69ce43a85eb193bca7313757b21faee
Cam <github@lekienhoanh.dev>
2022-06-21 17:02:11 +0700

    create Customer Address model and Migration

---
+++ database/migrations/2022_06_21_095918_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('customer_id')->nullable();
+            $table->string('address');
+            $table->integer('city_id');
+            $table->integer('district_id');
+            $table->integer('ward_id');
+            $table->string('name');
+            $table->string('phone');
+            $table->foreign('customer_id')->references('id')->on('customer_informations')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
