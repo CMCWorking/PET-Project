23ed468994c6de73a9f5a983403611e848318eb0
Cam <github@lekienhoanh.dev>
2022-06-22 09:02:43 +0700

    added Seeders to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use Database\Seeders\AddressSeeder;
+use Database\Seeders\CustomerInformationSeeder;
+use Database\Seeders\PaymentMethodSeeder;
+use Database\Seeders\StatusSeeder;
+use Database\Seeders\UserSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +19,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(UserSeeder::class);
+        $this->call(StatusSeeder::class);
+        $this->call(CustomerInformationSeeder::class);
+        $this->call(AddressSeeder::class);
+        $this->call(PaymentMethodSeeder::class);
+        $this->call(CategorySeeder::class);
     }
 }
